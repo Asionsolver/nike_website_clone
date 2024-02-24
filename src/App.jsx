c4db437ef55f6d48dcda4fadaba55e82e26c68a2
asionsolver <asionsolver274@gmail.com>
2024-02-24 11:07:58 +0600

    add some new section name like Nav, Hero Popular Product, Super Quality and some other section skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
-  );
-}
+const App = () => (
+  <main className="relative">
+  Nav
+  <section className="xl:padding-1 wide:padding-r padding-b">
+    Hero
+  </section>
+  <section className="padding">
+    Popular Products
+  </section>
+  <section className="padding">
+    Super Quality
+  </section>
+  <section className="padding-x py-10">
+    Services
+  </section>
+  <section className="padding">
+    Special Offers
+  </section>
+  <section className=" bg-pale-blue padding">
+    Customer Review
+  </section>
+  <section className="padding-x sm:py-32 py-16 w-full bg-gray-300">
+    Subscribe
+  </section>
+  <section className="bg-black padding-y padding-t pb-8 text-white">
+    Footer
+  </section>
+  </main>
+);
 
 export default App;
