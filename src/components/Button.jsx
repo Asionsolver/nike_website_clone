e8797ae8dd59b7034745e45735a757f36dd27e82
asionsolver <asionsolver274@gmail.com>
2024-02-27 22:33:07 +0600

    Complete Hero Section

---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+export const Button = ({ label, iconUrl }) => {
+  return (
+    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+      {label} 
+      <img className="ml-2 rounded-full w-5 h-5" src={iconUrl} alt="arrowRight-icon" />
+    </button>
+  );
+};
